1bba30fdee5941529220e7f6de07f2583544ecff
riccio82 <riccio82@gmail.com>
2020-04-01 23:35:00 +0200

    wip i18n

--- src/components/VideoSection/index.js
+++ src/components/VideoSection/index.js
@@ -1,27 +1,43 @@
 import React from 'react'
+import { FormattedMessage, injectIntl } from 'react-intl'
 import videoRight from '../../assets/images/screens/video-right.png'
 import videoLeft from '../../assets/images/screens/video-left.png'
 
-const VideoSection = () => (
+const VideoSection = (props) => (
   <div className='container-fluid col-sm-12 blue-soft-bkg' id='video-container'>
-  <div className='row justify-content-center'>
-      <div className='col-2'>
-          <img src={videoLeft} height='375' width='164'/>
-      </div>
-      <div className='col-8' style={{zIndex:3}}>
-          <div className='iframe-container'>
-              <iframe width='640' height='375' src='https://www.youtube.com/embed/k2OcIFyb9GE'
-                      frameBorder='0'
-                      allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-                      allowFullScreen></iframe>
-          </div>
+    <div className='js-slider row pb-5'>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/bocconi.png'/></div>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/cnr.png'/></div>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/copernicani.png'/></div>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/pi_campus.png'/></div>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/sapienza.png'/></div>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/translated.png'/></div>
+      <div className='js-slider-item'><img className='js-item' src='./CovidApp_files/screens/ufsc.png'/></div>
+    </div>
+    <div className='row justify-content-center'>
+        <div className='col-2'>
+            <img src={videoLeft} height='375' width='164'/>
+        </div>
+        <div className='col-8' style={{zIndex:3}}>
+            <div className='iframe-container'>
+              { props.intl.locale === 'it' ? (
+                <iframe width='640' height='375' src='https://www.youtube.com/embed/k2OcIFyb9GE'
+                        frameBorder='0'
+                        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+                        allowFullScreen/>
+              ) :(<iframe width="640" height="375" src="https://www.youtube.com/embed/l78iazHl_W8"
+                          frameBorder="0"
+                          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen/>)}
 
-      </div>
-      <div className='col-2'>
-          <img src={videoRight} height='375' width='164'/>
-      </div>
+            </div>
+
+        </div>
+        <div className='col-2'>
+            <img src={videoRight} height='375' width='164'/>
+        </div>
+    </div>
   </div>
-</div>
 )
 
-export default VideoSection
+export default injectIntl(VideoSection)
