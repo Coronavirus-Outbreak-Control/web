19066bbdca1fa364da115a2bdbce31606a96cee7
riccio82 <riccio82@gmail.com>
2020-04-02 22:46:34 +0200

    slider

--- src/components/VideoSection/index.js
+++ src/components/VideoSection/index.js
@@ -2,9 +2,44 @@ import React from 'react'
 import { injectIntl } from 'react-intl'
 import videoRight from '../../assets/images/screens/video-right.png'
 import videoLeft from '../../assets/images/screens/video-left.png'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+import bocconi from '../../assets/images/screens/bocconi.png';
+import cnr from '../../assets/images/screens/cnr.png';
+import copernicani from '../../assets/images/screens/copernicani.png';
+import pi_campus from '../../assets/images/screens/pi_campus.png';
+import sapienza from '../../assets/images/screens/sapienza.png';
+import translated from '../../assets/images/screens/translated.png';
+import ufsc from '../../assets/images/screens/ufsc.png';
+
+import Slider from "react-slick";
+
+const settings = {
+  speed: 800,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 4,
+  centerMode: true,
+  slidesToScroll: 1,
+  className: 'row pb-5',
+  centerPadding: '150px'
+
+};
 const VideoSection = (props) => (
   <div className='container-fluid col-sm-12 blue-soft-bkg' id='video-container'>
+    <div >
+      <Slider {...settings} >
+        <div><img className='js-item' src={bocconi} width="100px" height="100px"/></div>
+        <div><img className='js-item' src={cnr} width="100px" height="100px"/></div>
+        <div><img className='js-item' src={copernicani} width="100px" height="100px"/></div>
+        <div><img className='js-item' src={pi_campus} width="100px" height="100px"/></div>
+        <div><img className='js-item' src={sapienza} width="100px" height="100px"/></div>
+        <div><img className='js-item' src={translated} width="100px" height="100px"/></div>
+        <div><img className='js-item' src={ufsc} width="100px" height="100px"/></div>
+      </Slider>
+    </div>
     <div className='row justify-content-center'>
         <div className='col-2'>
             <img src={videoLeft} height='375' width='164'/>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './global/styles/normalize.css'
 import './global/styles/typography.css'
 import './global/styles/general.css'
 import './global/styles/bootstrap.min.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import * as serviceWorker from './serviceWorker'
 
 render(<App/>, document.getElementById('root'))
