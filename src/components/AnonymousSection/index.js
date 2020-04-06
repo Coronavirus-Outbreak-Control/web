855d801171ec00eacc8b310a252b75a22f070526
riccio82 <riccio82@gmail.com>
2020-04-06 13:45:20 +0200

    wip mobile

--- src/components/AnonymousSection/index.js
+++ src/components/AnonymousSection/index.js
@@ -1,45 +1,42 @@
 import React from 'react'
 import anonymous from '../../assets/images/screens/anonymous.png'
 import {FormattedMessage} from "react-intl";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 
 const AnonymousSection = () => (
   <div className='container-fluid col-sm-12 pt-5 pb-5 ' id='anonymous-container'>
-        <div className='row justify-content-md-center'>
-            <div className='col-2'></div>
-            <div className='col-8'>
+        <Row className='justify-content-md-center'>
+            <Col >
                 <h1><FormattedMessage id='Anonymous.title' /></h1>
                 <br/>
                 <p style={{fontSize: '18px'}}><FormattedMessage id='Anonymous.text' /></p>
                 <br/>
-            </div>
-            <div className='col-2'></div>
-        </div>
-        <div className='row'>
-            <div className='col-2'></div>
-            <div className='col-3'>
+            </Col>
+        </Row>
+        <Row >
+            <Col>
                 <h5><FormattedMessage id='Anonymous.subtitle' /></h5>
                 <br/>
                 <p><span className='dont-span'><FormattedMessage id='Anonymous.not' /></span><FormattedMessage id='Anonymous.text1' /></p>
                 <p><span className='dont-span'><FormattedMessage id='Anonymous.not' /></span><FormattedMessage id='Anonymous.text2' /></p>
                 <p><span className='dont-span'><FormattedMessage id='Anonymous.not' /></span><FormattedMessage id='Anonymous.text3' /></p>
                 <p><span className='dont-span'><FormattedMessage id='Anonymous.not' /></span><FormattedMessage id='Anonymous.text4' /></p>
-            </div>
-            <div className='col-7'>
-                <img src={anonymous} height='310' style={{marginLeft: '80px', marginTop: '-18px'}}/>
-            </div>
-        </div>
-        <div className='row justify-content-md-center'>
-            <div className='col-2'/>
-            <div className='col-8'>
+            </Col>
+            <Col className="image-container">
+                <img src={anonymous}/>
+            </Col>
+        </Row>
+        <Row className='justify-content-md-center'>
+            <Col>
                 <br/>
                 <p style={{fontSize: '18px'}}><FormattedMessage id='Anonymous.text5' />
                 <a href='https://github.com/Coronavirus-Outbreak-Control' target='_blank'>
                   Coronavirus Outbreak Control
                 </a></p>
-            </div>
-            <div className='col-2'/>
-        </div>
+            </Col>
+        </Row>
     </div>
 )
 
